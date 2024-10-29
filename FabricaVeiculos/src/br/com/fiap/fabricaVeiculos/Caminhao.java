5dae6ebe02882db70bcb968cb015e9fbd6bca88f
ErosMachado <erosrodriguesmachado@gmail.com>
2024-10-29 15:54:33 -0300

    resolução da parte 2

--- FabricaVeiculos/src/br/com/fiap/fabricaVeiculos/Caminhao.java
+++ FabricaVeiculos/src/br/com/fiap/fabricaVeiculos/Caminhao.java
@@ -1,5 +1,74 @@
 package br.com.fiap.fabricaVeiculos;
 
-public class Caminhao {
+import java.util.Date;
 
+public class Caminhao {
+	String marca, modelo, cor, motor, tipoCambio, tipoMotor, tipoCarga, nomeCondutor;
+	Date anoFabricacao = new Date();
+	Date anoModelo = new Date();
+	int renavam, eixos;
+	double aroRodas, velocidade, limiteVelocidade, capacidadeCarga, cargaAtual;
+	boolean ligado;
+
+	public void ligar() {
+		if (!this.ligado) {
+			this.ligado = true;
+		}
+	}
+
+	public void desligar() {
+		if (this.velocidade == 0) {
+			this.ligado = false;
+		}
+	}
+
+	public void acelerar(double valor) {
+		if (this.ligado) {
+			this.velocidade += valor;
+			if (this.velocidade > this.limiteVelocidade) {
+				this.velocidade = this.limiteVelocidade;
+			}
+		}
+	}
+
+	public void frear(double valor) {
+		if (this.velocidade > 0) {
+			this.velocidade -= valor;
+			if (this.velocidade < 0) {
+				this.velocidade = 0;
+			}
+		}
+	}
+
+	public void carregar(double valor) {
+		if (this.cargaAtual + valor <= this.capacidadeCarga) {
+			this.cargaAtual += valor;
+		}
+	}
+
+	public void descarregar(double valor) {
+		if (valor <= this.cargaAtual) {
+			this.cargaAtual -= valor;
+		}
+	}
+
+	public void exibirDados() {
+		System.out.println("Dados do Caminhão:");
+		System.out.println("Marca: " + this.marca);
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Ano de Fabricação: " + this.anoFabricacao);
+		System.out.println("Ano do Modelo: " + this.anoModelo);
+		System.out.println("Renavam: " + this.renavam);
+		System.out.println("Motor: " + this.motor);
+		System.out.println("Tipo de Câmbio: " + this.tipoCambio);
+		System.out.println("Aro das Rodas: " + this.aroRodas);
+		System.out.println("Quantidade de Eixos: " + this.eixos);
+		System.out.println("Capacidade de Carga: " + this.capacidadeCarga + " toneladas");
+		System.out.println("Carga Atual: " + this.cargaAtual + " toneladas");
+		System.out.println("Tipo de Carga: " + this.tipoCarga);
+		System.out.println("Nome do Condutor: " + this.nomeCondutor);
+		System.out.println("Status: " + (ligado ? "Ligado" : "Desligado"));
+		System.out.println("Velocidade Atual: " + this.velocidade + " km/h");
+	}
 }
---
+++ FabricaVeiculos/src/br/com/fiap/fabricaVeiculos/TesteCaminhao.java
@@ -0,0 +1,23 @@
+package br.com.fiap.fabricaVeiculos;
+
+import java.util.Date;
+
+public class TesteCaminhao {
+	public static void main(String[] args) {
+		Caminhao caminhao = new Caminhao();
+		caminhao.marca = "Volvo";
+		caminhao.modelo = "FH";
+		caminhao.cor = "Branco";
+		caminhao.renavam = 123456789;
+		caminhao.motor = "13L";
+		caminhao.tipoCambio = "Automático";
+		caminhao.aroRodas = 22.5;
+		caminhao.eixos = 3;
+		caminhao.capacidadeCarga = 25.0; 
+		caminhao.limiteVelocidade = 120.0; 
+		caminhao.tipoCarga = "Grãos";
+		caminhao.nomeCondutor = "Eros Machado";
+		
+		caminhao.exibirDados();
+	}
+}
