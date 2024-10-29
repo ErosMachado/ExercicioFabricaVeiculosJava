13b6bdf47358e1e24b6748b11c05b9c8e2da4380
ErosMachado <erosrodriguesmachado@gmail.com>
2024-10-29 15:09:11 -0300

    versão professor (inicial)

--- FabricaVeiculos/src/br/com/fiap/fabricaVeiculos/Carro.java
+++ FabricaVeiculos/src/br/com/fiap/fabricaVeiculos/Carro.java
@@ -1,92 +1,55 @@
 package br.com.fiap.fabricaVeiculos;
 
+import java.util.Date;
 
 public class Carro {
-    private String marca;
-    private String modelo;
-    private String cor;
-    private int anoFabricacao;
-    private int anoModelo;
-    private String renavam;
-    private String motor;
-    private String tipoCambio;
-    private int aroRodas;
-    private int velocidade;
-    private String nomeCondutor;
-    private boolean ligado;
+	String marca, motor, modelo, cor, tipoCambio, tipoMotor, nomeCondutor;
+	Date anoFabricacao = new Date();
+	Date anoModelo = new Date();
+	int renavam;
+	double aroRodas, velocidade;
+	boolean ligado;
 
-    public Carro(String marca, String modelo, String cor, int anoFabricacao, int anoModelo, 
-                 String renavam, String motor, String tipoCambio, int aroRodas, String nomeCondutor) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.cor = cor;
-        this.anoFabricacao = anoFabricacao;
-        this.anoModelo = anoModelo;
-        this.renavam = renavam;
-        this.motor = motor;
-        this.tipoCambio = tipoCambio;
-        this.aroRodas = aroRodas;
-        this.velocidade = 0;
-        this.nomeCondutor = nomeCondutor;
-        this.ligado = false;
-    }
+	public void ligar() {
+		if (!this.ligado) {
+			this.ligado = true;
+		}
+	}
 
-    public void ligar() {
-        if (!ligado) {
-            ligado = true;
-            System.out.println("Carro ligado.");
-        } else {
-            System.out.println("O carro já está ligado.");
-        }
-    }
+	public void desligar() {
+		if (this.velocidade == 0) {
+				this.ligado = false;
+		}
+	}
 
-    public void desligar() {
-        if (ligado && velocidade == 0) {
-            ligado = false;
-            System.out.println("Carro desligado.");
-        } else if (velocidade != 0) {
-            System.out.println("O carro precisa estar parado para desligar.");
-        } else {
-            System.out.println("O carro já está desligado.");
-        }
-    }
+	public void acelerar(double valor) {
+		if (this.ligado) {
+			this.velocidade += valor;
+		}
+	}
 
-    public void acelerar(int incremento) {
-        if (ligado) {
-            velocidade += incremento;
-            System.out.println("Acelerando. Velocidade atual: " + velocidade + " km/h.");
-        } else {
-            System.out.println("O carro precisa estar ligado para acelerar.");
-        }
-    }
+	public void frear(double valor) {
+		if (this.velocidade > 0) {
+			velocidade -= valor;
+		}
+		 if (this.velocidade < 0) {
+             this.velocidade = 0;
+         }
+	}
 
-    public void frear(int decremento) {
-        if (ligado && velocidade > 0) {
-            velocidade -= decremento;
-            if (velocidade < 0) {
-                velocidade = 0;
-            }
-            System.out.println("Freando. Velocidade atual: " + velocidade + " km/h.");
-        } else if (!ligado) {
-            System.out.println("O carro precisa estar ligado para frear.");
-        } else {
-            System.out.println("O carro já está parado.");
-        }
-    }
-
-    public void exibirDados() {
-        System.out.println("Dados do Carro:");
-        System.out.println("Marca: " + marca);
-        System.out.println("Modelo: " + modelo);
-        System.out.println("Cor: " + cor);
-        System.out.println("Ano de Fabricação: " + anoFabricacao);
-        System.out.println("Ano do Modelo: " + anoModelo);
-        System.out.println("Renavam: " + renavam);
-        System.out.println("Motor: " + motor);
-        System.out.println("Tipo de Câmbio: " + tipoCambio);
-        System.out.println("Aro das Rodas: " + aroRodas);
-        System.out.println("Velocidade Atual: " + velocidade + " km/h");
-        System.out.println("Nome do Condutor: " + nomeCondutor);
-        System.out.println("Status: " + (ligado ? "Ligado" : "Desligado"));
-    }
+	public void exibirDados() {
+		System.out.println("Dados do Carro:");
+		System.out.println("Marca: " + this.marca);
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Cor: " + this.cor);
+		System.out.println("Ano de Fabricação: " + this.anoFabricacao);
+		System.out.println("Ano do Modelo: " + this.anoModelo);
+		System.out.println("Renavam: " + this.renavam);
+		System.out.println("Motor: " + this.motor);
+		System.out.println("Tipo de Câmbio: " + this.tipoCambio);
+		System.out.println("Aro das Rodas: " + this.aroRodas);
+		System.out.println("Velocidade Atual: " + this.velocidade + " km/h");
+		System.out.println("Nome do Condutor: " + this.nomeCondutor);
+		System.out.println("Status: " + (ligado ? "Ligado" : "Desligado"));
+	}
 }
